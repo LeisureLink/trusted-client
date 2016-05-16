a21ca409be26bf0a279ad054cb7e747283c5a686
Jesse Hansen <twindagger@gmail.com>
2016-05-16 13:51:06 -0600

    Label instead of description - nicer output on error

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -14,11 +14,11 @@ export default {
   validate,
   trustedClient,
   routedClient,
-  requiredAny: name => joi.any().description(name).required(),
-  requiredString: name => joi.string().description(name).required(),
-  optionalString: name => joi.string().description(name).allow(null),
-  requiredObject: (name, keys) => joi.object(keys).description(name).required(),
-  optionalObject: (name, keys) => joi.object(keys).description(name).allow(null),
-  requiredFunc: name => joi.func().description(name).required(),
-  optionalFunc: name => joi.func().description(name).allow(null)
+  requiredAny: name => joi.any().label(name).required(),
+  requiredString: name => joi.string().label(name).required(),
+  optionalString: name => joi.string().label(name).allow(null),
+  requiredObject: (name, keys) => joi.object(keys).label(name).required(),
+  optionalObject: (name, keys) => joi.object(keys).label(name).allow(null),
+  requiredFunc: name => joi.func().label(name).required(),
+  optionalFunc: name => joi.func().label(name).allow(null)
 };
